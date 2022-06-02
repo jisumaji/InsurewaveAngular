c457f61582a64e5055fbd27e99ef63413edbf70b
SWETA SARKAR <sarkarsweta663@gmail.com>
2022-06-03 01:49:27 +0530

    addassets

---
+++ src/app/users/buyer/add-assets/add-assets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-assets',
+  templateUrl: './add-assets.component.html',
+  styleUrls: ['./add-assets.component.css']
+})
+export class AddAssetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/users/buyer/display-assets/display-assets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-assets',
+  templateUrl: './display-assets.component.html',
+  styleUrls: ['./display-assets.component.css']
+})
+export class DisplayAssetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
