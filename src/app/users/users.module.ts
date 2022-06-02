0aacb21b272bb6155c811ee67476fe3d875fab21
SWETA SARKAR <sarkarsweta663@gmail.com>
2022-06-03 01:05:04 +0530

    added components

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,11 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { Error404Component } from './error404/error404.component';
+import { AddAssetsComponent } from './buyer/add-assets/add-assets.component';
+import { DisplayAssetsComponent } from './buyer/display-assets/display-assets.component';
+import { PaymentsComponent } from './buyer/payments/payments.component';
+import { RequestsComponent } from './broker/requests/requests.component';
+import { PoliciesComponent } from './broker/policies/policies.component';
+import { PolicyComponent } from './insurer/policy/policy.component';
 
 
 @NgModule({
   declarations: [
-    Error404Component
+    Error404Component,
+    AddAssetsComponent,
+    DisplayAssetsComponent,
+    PaymentsComponent,
+    RequestsComponent,
+    PoliciesComponent,
+    PolicyComponent
   ],
   imports: [
     CommonModule,
