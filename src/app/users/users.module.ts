b5993994f1af9affeba4df2d3eec15cd60deb1d5
jisu <jisumaji@gmail.com>
2022-08-03 02:25:43 +0530

    text color

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -3,16 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule,routes } from './users-routing.module';
 import { Error404Component } from './error404/error404.component';
-import { RequestsComponent } from './broker/requests/requests.component';
-import { PoliciesComponent } from './broker/policies/policies.component';
 import { PolicyComponent } from './insurer/policy/policy.component';
 import {RouterModule} from '@angular/router'
 
 @NgModule({
   declarations: [
     Error404Component,
-    RequestsComponent,
-    PoliciesComponent,
     PolicyComponent
   ],
   imports: [
