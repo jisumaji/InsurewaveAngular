fff2a301d1c39c64fccf7408e961d228218eb8b4
jisu <jisumaji@gmail.com>
2022-08-10 00:40:09 +0530

    updated angular

--- src/app/users/broker/broker-nav/broker-nav.component.ts
+++ src/app/users/broker/broker-nav/broker-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-broker-nav',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrokerNavComponent implements OnInit {
 
+  @Input() brokerName:string='';
   constructor() { }
 
   ngOnInit(): void {
--- src/app/users/broker/broker.component.ts
+++ src/app/users/broker/broker.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserDetailsService } from '../../services/user-details.service';
+import { IUserDetails } from '../../models/userDetails.model';
 @Component({
   selector: 'app-broker',
   templateUrl: './broker.component.html',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrokerComponent implements OnInit {
 
-  constructor() { }
+  private brokerData: IUserDetails = {
+    userId: "null",
+    password: "null",
+    firstName: "null",
+    lastName: "null",
+    gender: "male",
+    role: "buyer",
+    licenseId: -1,
+  }
+  brokerName: string = '';
+  constructor(private user: UserDetailsService) { }
 
   ngOnInit(): void {
+    this.user.emitData.subscribe(result => {
+      // console.log(result);
+      this.brokerData.userId = result.userId;
+      this.brokerData.password = result.password;
+      this.brokerData.firstName = result.firstName;
+      this.brokerData.lastName = result.lastName;
+      this.brokerData.gender = result.gender;
+      this.brokerData.role = result.role;
+      this.brokerData.licenseId = result.licenseId;
+      this.brokerName = this.brokerData.firstName;
+    });
+
   }
 
 }
